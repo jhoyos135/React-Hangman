df32369ae58ea92f783ed461098997fa4bf33cef
Julian Hoyos <jhoyos135@gmail.com>
2019-04-29 13:11:44 -0400

    added images folder, gameover logic and refactor

--- src/Hangman.js
+++ src/Hangman.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 import "./Hangman.css";
-import img0 from "./0.jpg";
-import img1 from "./1.jpg";
-import img2 from "./2.jpg";
-import img3 from "./3.jpg";
-import img4 from "./4.jpg";
-import img5 from "./5.jpg";
-import img6 from "./6.jpg";
+import img0 from "./images/0.jpg";
+import img1 from "./images/1.jpg";
+import img2 from "./images/2.jpg";
+import img3 from "./images/3.jpg";
+import img4 from "./images/4.jpg";
+import img5 from "./images/5.jpg";
+import img6 from "./images/6.jpg";
 
 class Hangman extends Component {
-  /** by default, allow 6 guesses and use provided gallows images. */
+
   static defaultProps = {
     maxWrong: 6,
     images: [img0, img1, img2, img3, img4, img5, img6]
@@ -21,19 +21,12 @@ class Hangman extends Component {
     answer: "apple" 
   };
 
-  /** guessedWord: show current-state of word:
-    if guessed letters are {a,p,e}, show "app_e" for "apple"
-  */
   guessedWord = () => {
     return this.state.answer
       .split("")
       .map(ltr => (this.state.guessed.has(ltr) ? ltr : "_"));
   };
 
-  /** handleGuest: handle a guessed letter:
-    - add to guessed letters
-    - if not in answer, increase number-wrong guesses
-  */
   handleGuess = (evt) => {
     let ltr = evt.target.value;
     this.setState(st => ({
@@ -42,10 +35,10 @@ class Hangman extends Component {
     }));
   };
 
-  /** generateButtons: return array of letter buttons to render */
   generateButtons = () => {
     return "abcdefghijklmnopqrstuvwxyz".split("").map(ltr => (
       <button
+        key={ltr}
         value={ltr}
         onClick={this.handleGuess}
         disabled={this.state.guessed.has(ltr)}
@@ -55,14 +48,20 @@ class Hangman extends Component {
     ));
   };
 
-  /** render: render game */
   render() {
+    const gameOver = this.state.nWrong >= this.props.maxWrong;
     return (
       <div className='Hangman'>
         <h1>Hangman</h1>
         <img src={this.props.images[this.state.nWrong]} />
-        <p className='Hangman-word'>{this.guessedWord()}</p>
-        <p className='Hangman-btns'>{this.generateButtons()}</p>
+        <p>Wrong guesses: { this.state.nWrong }</p>
+        <p className='Hangman-word'>
+          {!gameOver ? this.guessedWord() : this.state.answer}
+          </p>
+        <p className='Hangman-btns'>
+          {!gameOver ? this.generateButtons() :
+          `You lose the answer was: ${this.state.answer}`}
+          </p>
       </div>
     );
   }
